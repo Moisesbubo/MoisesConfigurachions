ef4d35bb356f3df17126fbad907e3e9595157fc6
Moisesbubo <moiseslopes7852@gmail.com>
2025-05-11 17:48:20 -0400

    Vi motion

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,6 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-    set -l random_image (ls ~/Imagens/Ana_photos/* | shuf -n 1)
-    jp2a --colors --width=40 $random_image
-    set -U fish_greeting "Hello everyone"
+function fish_user_key_bindings
+    fish_vi_key_bindings
+
+    # complete
+    bind -M insert \cf forward-char 
 end
