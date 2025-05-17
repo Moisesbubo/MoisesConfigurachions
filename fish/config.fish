c9e0b303e5a336082b52310ae3dffe59fe1c2b4a
Moisesbubo <moiseslopes7852@gmail.com>
2025-05-17 12:00:40 -0400

    Frufru files added

--- fish/config.fish
+++ fish/config.fish
@@ -1,9 +1,10 @@
+#ASCII Image of Ana
 set -l random_image (ls ~/Imagens/Ana_photos/* | shuf -n 1)
 jp2a --colors --width=40 $random_image
 
 function fish_user_key_bindings
+#Set Vi Keybindings 
     fish_vi_key_bindings
-
-    # complete
+#Set ctrl f to complete 
     bind -M insert \cf forward-char 
 end
